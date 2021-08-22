ae1ea4f845f06f5575d1651157dfbbabc7c29fd8
zrk8856 <lelouch_l@icloud.com>
2021-08-23 01:22:19 +0800

    Expand on previous project with randomly changing dodger's color when clicking on it

---
+++ script.js
@@ -0,0 +1,32 @@
+const dodger = document.getElementById("dodger");
+
+function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+  }
+
+  function moveDodgerRight() {
+      const leftNumbers = dodger.style.left.replace("px", "");
+      const left = parseInt(leftNumbers, 10);
+      if (left < 360) {
+          dodger.style.left = `${left + 1}px`;
+      }
+  }
+
+  document.addEventListener("keydown", function(e) {
+    if (e.key === "ArrowLeft") {
+      moveDodgerLeft();
+    } else if (e.key === "ArrowRight") {
+        moveDodgerRight();
+    }
+  });
+
+dodger.addEventListener("click", function() {
+    randomColor = Math.floor(Math.random()*16777215).toString(16);
+    dodger.style.backgroundColor = "#" + randomColor;
+}
+)
